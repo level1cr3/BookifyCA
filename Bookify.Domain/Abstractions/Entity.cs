70c5fdc100b123b12887e8781dac15dfe473d38b
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-07-19 06:26:34 +0530

    Intial commit

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity(Guid id)
+{
+    public Guid Id { get; init; } = id;
+
+}
+
+
+// what is Entity ?
+// it is a object that has a unique identifier id. and it contineous that means existence of this object is important throught
+// the life of the application
