558f461feb243ea40b3982cd31e8c1a2017c99fb
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-07-21 09:55:01 +0530

    Using the static factory pattern in domain

--- Bookify.Domain/Apartments/Apartment.cs
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -30,3 +30,8 @@ public sealed class Apartment(Guid id,
 // The problem with strings or any premitive type which are part of your domain model is that they convey no meaning.
 // To solve premitive obsession and also improve the design of our domain model we are going to introduce value object to represent the address.
 // how we going to define the value objects. One that is really practical is using 'record type'.
+
+
+// Why do we have private setters on all of th properties ?
+// We don't want to let anyone change the values inside our entity from outside. If they could,
+// it would break the rules and protections we've put in place to keep our design safe and consistent.
---
+++ Bookify.Domain/Users/User.cs
@@ -0,0 +1,38 @@
+﻿using Bookify.Domain.Abstractions;
+using System;
+
+namespace Bookify.Domain.Users;
+
+public sealed class User : Entity
+{
+    private User(Guid id, FirstName firstName, LastName lastName, Email email)
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public FirstName FirstName { get; private set; }
+
+    public LastName LastName { get; private set; }
+
+    public Email Email { get; private set; }
+
+    public static User Create(FirstName firstName, LastName lastName, Email email)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+        return user;
+    }
+
+}
+
+
+// what is the benifit of wrapping the constructor inside a factory method ?
+// there are few benifits to using this approach
+// 1. hiding your constructor which could have someother implementation details. that we don't to expose outside of the user entity
+// 2. encapsulation.
+// 3. to be able to introduce some side-effect inside the factory method that don't naturally don't belong inside the constructor.
+// // what we are talking about is domain events
+
