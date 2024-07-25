845b563095c826acce407c1acc50638cf413d35b
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-07-25 06:23:41 +0530

    Adding repositories and UnitOfWork in domain

---
+++ Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain.Apartments;
+public interface IApartmentRepository
+{
+    Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellation = default);
+}
+
+// we will use this later on application layer to execute the required business operation.
