5eb0e728433c65114e843a78880180a58356d053
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-08-22 06:11:23 +0530

    Implementing Queries with dapper

---
+++ Bookify.Application/Bookings/GetBooking/BookingResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace Bookify.Application.Bookings.GetBooking;
+public sealed class BookingResponse
+{
+    public Guid Id { get; init; }
+    
+    public Guid UserId { get; init; }
+    
+    public Guid ApartmentId { get; init; }
+    
+    public int Status { get; init; }
+    
+    public decimal PriceAmount { get; init; }
+    
+    public string PriceCurrency { get; init; }
+    
+    public decimal CleaningFeeAmount { get; init; }
+    
+    public string CleaningFeeCurrency { get; init; }
+    
+    public decimal AmenitiesUpChargeAmount { get; init; }
+    
+    public string AmenitiesUpChargeCurrency { get; init; }
+    
+    public decimal TotalPriceAmount { get; init; }
+    
+    public string TotalPriceCurrency { get; init; }
+    
+    public DateOnly DurationStart { get; init; }
+    
+    public DateOnly DurationEnd { get; init; }
+    
+    public DateTime CreatedOnUtc { get; init; }
+    
+}
+
+// for query responses we want to be using premitive types as much as possible. and we want a very flat strucutre which we can easily return from the database.
+// all of these properties are already present in bookings how ever they are hidden behind a rich domain model and value objects.
---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,61 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+{ 
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    { 
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    { 
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        // executing query with dapper is simple we just call one of the extensions methods that are exposed on db connection interface.
+
+        const string sql = """
+            SELECT 
+                id AS Id,
+                apartment_id AS ApartmentId,
+                user_id AS UserId,
+                status AS Status,
+                price_for_period_amount AS PriceAmount,
+                price_for_period_currency AS PriceCurrency,
+                cleaning_fee_amount AS CleaningFeeAmount,
+                cleaning_fee_currency AS CleaningFeeCurrency,
+                amenities_up_charge_amount AS AmenitiesUpChargeAmount,
+                amenities_up_charge_currency AS AmenitiesUpChargeCurrency,
+                total_price_amount AS TotalPriceAmount,
+                total_price_currency AS TotalPriceCurrency,
+                duration_start AS DurationStart,
+                duration_end AS DurationEnd,
+                created_on_utc AS CreatedOnUtc
+            FROM bookings
+            WHERE id = @BookingId
+            """;
+
+
+        var booking= await connection.QueryFirstOrDefaultAsync<BookingResponse>(sql, new {request.BookingId});
+        
+
+        return booking;
+    }
+}
+
+
+// This is a pragmatic clean architecture.
+// we are using sql to directly access our read model and directly return response from our query. without any indirection. Downside is that we are not completely
+// abstracting our persistence concerns in this case our sql database from our query handler But the benifits are too many to count.
+// 1. it is simple and performant because of dapper
+// 2. Being able to define your readmodel in database level by defining database view. (so you don't have write complecated query in code)
+
+// But what about not being able to switch a database?
+// If you are switching db then you have a lot of problem. then just using sql in application layer.
+// If you end up moving from sql to document database or column store. then you gonna have to write your entire persistence layer.
