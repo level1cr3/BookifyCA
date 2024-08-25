195842ba08d7bf595305bc0b89d0e21527b38a30
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-08-25 09:29:00 +0530

    Add: Infrastructure layer, add service for datetime, email.

---
+++ Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Application.Abstractions.Email;
+
+namespace Bookify.Infrastructure.Email;
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Domain.Users.Email recipient, string subject, string body)
+    {
+        // here we would implement email service
+
+        // but for toturial he didn't wanted to do it. we just simulate that this method completed successfully.
+        return Task.CompletedTask;
+    }
+}
