13b42f51daddab221fafb1ff2dbbf662d61b7498
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-08-27 06:28:33 +0530

    Add: Ef core and configure it with dependency injection

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +14,19 @@ public static class DependencyInjection
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
 
         services.AddTransient<IEmailService, EmailService>();
+
+
+        var connectionString = configuration.GetConnectionString("Database") ?? throw new ArgumentNullException(nameof(configuration));
+
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+
+            // ef core by convention is going to use 'title case' for names of the table and columns in your database model.
+            // postgres on the other hand perferes 'snake case'.
+            // for this reason we will use package called efcore naming convention.
+        });
 
         return services;
     }
