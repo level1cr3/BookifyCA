b7d69ba7f2b276db1285ce13034d6b9dad3891aa
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-09-21 07:27:45 +0530

    Add: persentation layer, wireup dependency injection

---
+++ Bookify.Api/Program.cs
@@ -0,0 +1,37 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// this is all we need to wireup dependency inejction. because we added the respective service registrations in the application and infrastructure project.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
