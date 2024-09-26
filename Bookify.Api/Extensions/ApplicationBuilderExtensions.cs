63aca57544f420ae7046a518cc0f1a477823de0d
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-09-27 05:27:44 +0530

    Add: Global exception handler middleware

--- Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Infrastructure;
+﻿using Bookify.Api.Middleware;
+using Bookify.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Api.Extensions;
@@ -8,11 +9,17 @@ public static class ApplicationBuilderExtensions
     public static void ApplyMigrations(this IApplicationBuilder app)
     {
         using var scope = app.ApplicationServices.CreateScope();
-        
+
         using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); // use scope to resolve the dbcontext.
-        
+
         dbContext.Database.Migrate();
     }
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+
 }
 
 // This method ApplyMigrations is going to be used only for local development process.
